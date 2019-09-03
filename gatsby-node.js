e8b9ab82b542f9d0dec1756c3e7de5d840a26639
Andrew Larcombe <andrew@andrewl.net>
2019-09-03 20:13:09 +0100

    Adds support for layers, filters and adds centroids and envelopes to output

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,6 +43,17 @@ exports.sourceNodes = (
     proxy_feature.defn = feature.defn;
     proxy_feature.featureFields = feature.fields.toObject();
     proxy_feature.geometry = feature.getGeometry().toObject();
+    var envelope = feature.getGeometry().getEnvelope();
+    proxy_feature.geometry.envelope = {
+      minX: envelope.minX,
+      minY: envelope.minY,
+      maxX: envelope.maxX,
+      maxY: envelope.maxY,
+    };
+    proxy_feature.geometry.centroid = {
+      x: feature.getGeometry().centroid().x,
+      y: feature.getGeometry().centroid().y
+    };
     proxy_feature.layer_name = layer.name;
 
     const nodeId = createNodeId(`geo-${feature.fid}`)
@@ -67,14 +78,41 @@ exports.sourceNodes = (
 
   //@todo - more options, including layer names etc
   var dataset = gdal.open(configOptions.path);
+  console.log(configOptions);
 
   dataset.layers.forEach(function(layer) {
-    const layerNode = processLayer(layer);
-    createNode(layerNode);
-    layer.features.forEach(function(feature) {
-      const featureNode = processFeature(feature, layer);
-      createNode(featureNode);
-    });
+    var include_this_layer = true;
+    //quick spin around to see if we should include this layer in the processing
+    if (typeof(configOptions.layers) != 'undefined') {
+      include_this_layer = false;
+      console.log(configOptions.layers);
+      configOptions.layers.forEach(function(layer_config) {
+        if (layer_config.name == layer.name) {
+          include_this_layer = true;
+          //apply the layer configuration
+          if (layer_config.attribute_filter) {
+            console.log("Applying attribute filter '"+layer_config.attribute_filter+"' to layer '" + layer.name + "'");
+            layer.setAttributeFilter(layer_config.attribute_filter);
+          }
+          if (layer_config.spatial_filter) {
+            console.log("Applying spatial filter to layer '" + layer.name + "'");
+            layer.setSpatialFilter(layer_config.attribute_filter);
+          }
+        }
+      });
+    }
+
+    if (!include_this_layer) {
+      console.log("Skipping layer " + layer.name);
+    }
+    else {
+      const layerNode = processLayer(layer);
+      createNode(layerNode);
+      layer.features.forEach(function(feature) {
+        const featureNode = processFeature(feature, layer);
+        createNode(featureNode);
+      });
+    }
   });
 
   return;
